901c233228493216602a58f9738e04fd54712b67
Midstackdev <midstackdev@gmail.com>
2020-05-17 00:02:37 +0000

    Sanctum set up for api response

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        {{ user }}
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+    export default {
+        data () {
+            return {
+                user: null
+            }
+        },
+        methods: {
+            async getUser () {
+                let response = await axios.get('/api/user')
+
+                this.user = response.data 
+            }
+        },
+        mounted() {
+            this.getUser()
+            console.log('Component mounted.')
+        }
+    }
+</script>
