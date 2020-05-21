9a9d98c4b5271b4e7bf29e5c68168bc3f48fda02
Midstackdev <midstackdev@gmail.com>
2020-05-21 20:22:39 +0000

    Setting up laravel websocket

--- app/Http/Controllers/Api/Tweets/TweetController.php
+++ app/Http/Controllers/Api/Tweets/TweetController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api\Tweets;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Tweets\TweetStoreRequest;
 use Illuminate\Http\Request;
 
 class TweetController extends Controller
